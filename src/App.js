285988386d6ac84ae49dcb0071ee51d866493bee
Vicky Banh <samsokhua1@gmail.com>
2018-08-27 19:52:51 -0400

    basic of using props

--- src/App.js
+++ src/App.js
@@ -8,13 +8,23 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Welcome text="Welcome to Using Props" />
         </header>
         <p className="App-intro">
+          {}
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
+  }
+}
+
+class Welcome extends Component {
+  render() {
+    const { text } = this.props;  // get the value of text in props
+    return (
+      <h1 className="App-title">{text}</h1>
+    )
   }
 }
 
