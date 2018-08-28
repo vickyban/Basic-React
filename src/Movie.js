a0f550cf7b8e0e63ff447060e89910fe0bcd1a53
Vicky Banh <samsokhua1@gmail.com>
2018-08-28 18:57:45 -0400

    adding transistion effect with react-overdrive package

--- src/Movie.js
+++ src/Movie.js
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Overdrive from 'react-overdrive';
 
 const POSTER_PATH = 'https://image.tmdb.org/t/p/w154';
 
 const Movie = ({ movie }) => (
   <Link to={`/movie/${movie.id}`}>
-    <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+    <Overdrive id={movie.id}>
+      <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+    </Overdrive>
   </Link>
 );
 
