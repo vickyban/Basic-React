6e94293d9b00f2df94e9406f2cc8c57ad57f303d
Vicky Banh <samsokhua1@gmail.com>
2018-08-28 12:25:44 -0400

    fetching api inside componentDidMount() and setState with the results

--- src/App.js
+++ src/App.js
@@ -3,29 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    id: 1,
-    title: 'Star Wars',
-    desc: 'A space movie'
-  }, {
-    id: 2,
-    title: 'Spider Man'
-  }, {
-    id: 3,
-    title: '36th Chamber of Shaolin'
-  }
-];
-
 class App extends Component {
+  state = {
+    movies: []
+  }
+
+  async componentDidMount() {
+    try {
+      const url = 'https://api.themoviedb.org/3/discover/movie?api_key=ebdaf01ed8d5831bedeb284c4c75a84b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1';
+      const res = await fetch(url);
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results
+      })
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie} desc={movie.desc} />)}
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -2,25 +2,17 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 export default class Movie extends Component {
-  // static because it's not gonna change
-  // shape allows to check the datatype of the object's properties
   static propTypes = {
     movie: PropTypes.shape({
       title: PropTypes.string.isRequired,
     }),
-    desc: PropTypes.string
   };
 
-  // can't set default value for nested properties (eg movie.desc)
-  static defaultProps = {
-    desc: 'Description not available'
-  };
 
   render() {
     return (
       <div>
         <h3>{this.props.movie.title}</h3>
-        <p>{this.props.desc}</p>
       </div>
     )
   }
