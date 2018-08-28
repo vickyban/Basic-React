3a9c9097dfa82632217e4bc4fb77248f6598faf5
Vicky Banh <samsokhua1@gmail.com>
2018-08-28 17:10:44 -0400

    styled-components to add css to components

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,5 +1,6 @@
 /* eslint react/no-did-mount-set-state: 0 */
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import Movie from './Movie';
 
 class MovieList extends Component {
@@ -22,11 +23,18 @@ class MovieList extends Component {
 
   render() {
     return (
-      <div>
+      <MovieGrid>
         {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
-      </div>
+      </MovieGrid>
     );
   }
 }
 
 export default MovieList;
+
+const MovieGrid = styled.div`
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+  grid-row-gap: 1rem;
+`;
